caa6cd16f83a3d8607dcc8f6664abe5be89485ac
ElinorSel <124194603+ElinorSel@users.noreply.github.com>
2026-09-02 11:58:38 +0200

    final changes example

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,8 +12,14 @@ function App() {
   const [isShown2, setIsShown2] = useState(false);
   return (
     <>
+    <div>
+      <h1>Lazy loading demo</h1>
+      <p>Lazy Component: The component is lazy loaded and therefore its JavaScript is only loaded when the button is pressed, as can be seen in the Network tab.</p>
+      <p>Excited Component: The component is not lazy loaded, so its JavaScript is included in the initial page load and is already visible in the Network tab. </p>
+      <p>note: The both images appear on the button clicks as they are requested on click </p>
+    </div>
       <button onClick={() => setIsShown(!isShown)}>Lazy Component</button>
-      {isShown && <Suspense fallback = {<span className="loader"></span>}>
+      {isShown && <Suspense fallback = "Loading...">
         <LazyComponent />
       </Suspense>}
       <button onClick={() => setIsShown2(!isShown2)}>Excited Component</button>
